ab8c67ee442c8076342145c592e68e77661d5f7d
PabloOrtegaC <78177335+PabloOrtegaC@users.noreply.github.com>
2025-03-12 23:14:43 -0500

    rama parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import LoginPage from './components/Login/LoginPage';
-import RegisterPage from './components/Login/RegisterPage';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} /> */
+
     </Routes>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import Header from './Header/Header';
-import Matrix from './Columns/Columns';
-import ImageCard from './ImageCard/ImageCard';
-import Footer from './Footer/Footer';
-import Table from './Table/Table'
+
 
 
 
@@ -11,37 +7,7 @@ const Home = () => {
   return (
 
     <div>
-      <Header />
-      <Matrix columns={3} cellWidth={300} cellHeight={150}>
-      <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-            <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-            <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-            <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-            <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-            <ImageCard 
-        backgroundUrl=  'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-        text="Welcome to Our Website"
-      />
-        
-    </Matrix>
-    <Table></Table>
-    <Footer></Footer>
-
+     Hello world
       
     </div>
   );
